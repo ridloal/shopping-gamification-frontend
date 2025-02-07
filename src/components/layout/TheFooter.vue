c437c05fff2a4ef0d237967226babb5e09a3e4f5
Ridlo Alimudin <ridlo.alimudin@gmail.com>
2025-02-07 20:06:56 +0700

    Refactor footer component to use localized links for improved internationalization support

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,31 +1,37 @@
 <template>
-    <footer>
-      <div class="footer-links">
-        <a v-for="link in links" :key="link.text" :href="link.url">
-          {{ link.text }}
-        </a>
-      </div>
-      <p>&copy; {{ currentYear }} HartaKarun. All rights reserved.</p>
-    </footer>
-  </template>
-  
-  <script>
-  export default {
-    name: 'TheFooter',
-    data() {
-      return {
-        links: [
-          { text: this.$t('About Us'), url: '/about-us' },
-          { text: this.$t('Terms'), url: '/terms' },
-          { text: this.$t('Privacy'), url: '/privacy' },
-          { text: this.$t('Contact Us'), url: '/contact-us' }
-        ]
-      }
+  <footer>
+    <div class="footer-links">
+      <a v-for="link in localizedLinks" :key="link.text" :href="link.url">
+        {{ link.text }}
+      </a>
+    </div>
+    <p>&copy; {{ currentYear }} HartaKarun. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+  data() {
+    return {
+      links: [
+        { text: 'About Us', url: '/about-us' },
+        { text: 'Terms', url: '/terms' },
+        { text: 'Privacy', url: '/privacy' },
+        { text: 'Contact Us', url: '/contact-us' }
+      ]
+    }
+  },
+  computed: {
+    currentYear() {
+      return new Date().getFullYear()
     },
-    computed: {
-      currentYear() {
-        return new Date().getFullYear()
-      }
+    localizedLinks() {
+      return this.links.map(link => ({
+        ...link,
+        text: this.$t(link.text)
+      }))
     }
   }
-  </script>
+}
+</script>
